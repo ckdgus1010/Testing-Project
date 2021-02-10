89f2e611b508fb3c1f247f80130e4fba5a8f87fd
on_off <63770983+ckdgus1010@users.noreply.github.com>
2021-02-10 13:55:08 +0900

    Initial commit

---
+++ Assets/02. Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Transform slot;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Transform newSlot = Instantiate(slot);
+                newSlot.name = "Slot" + (i + 1) + "." + (j + 1);
+                newSlot.parent = transform;
+
+                RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+                slotRect.anchorMin = new Vector2(0.2f * j + 0.05f, 1 - (0.2f * (i + 1) - 0.05f));
+                slotRect.anchorMax = new Vector2(0.2f * (j + 1) - 0.05f, 1 - (0.2f * i + 0.05f));
+                slotRect.offsetMin = Vector2.zero;
+                slotRect.offsetMax = Vector2.zero;
+            }
+        }
+    }
+}
